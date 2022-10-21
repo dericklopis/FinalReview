f843aac00baba8cfec663fb541f0b224a42130ab
dericklopis <stanforever08@gmail.com>
2022-10-21 21:04:00 +0530

    Added UC1

--- coin.sh
+++ coin.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-Flip=$(($RANDOM%2));
-        if [ $Flip -eq 1 ]
-then
-        echo "heads"
-else
-        echo "tails"
+Flip=$(($RANDOM%10))%2))
+if [ $Flip -eq 1 ]
+      then
+        echo "Heads"
+      else
+        echo "Tails"
 fi
-
-echo $
